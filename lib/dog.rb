edc416180109dbbed4280f4d4e33fa64b72fca8a
Malcolm Leake <echelon1089@gmail.com>
2019-02-26 00:53:03 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+    
+    def sit  
+      @sitting = puts "The Dog is sitting"
+    end
+    
+  
+  
+end 
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  def talk
+    @talking = puts "Hello World!"
+  end 
+  
+    def walk
+      @walking = puts "The Person is walking"
+      end
+  
+  
+end 
